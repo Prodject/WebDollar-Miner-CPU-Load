b729f07d7c0cd18139dd8aa6a711928b83a1023e
Constantin Busuioceanu <39584380+cbusuioceanu@users.noreply.github.com>
2018-10-04 21:39:24 +0300

    Update minercpuload.sh
    -> remove /tmp files after the miner is restarted

--- minercpuload.sh
+++ minercpuload.sh
@@ -11,16 +11,16 @@ if [[ ! $(uname -r | cut -d '-' -f3) == Microsoft ]]; then
 		if [[ $cpu_usage -le 25  ]]; then
 			echo "Low_CPU_usage_detected: $cpu_usage%"
 			echo "Sending CTRL+C..." && screen -S $miner_screen_name -p 0 -X stuff "^C" && sleep 2
-			echo "Starting npm run commands..." && screen -S $miner_screen_name -p 0 -X stuff "SERVER_PORT=$miner_server_port npm run commands^M"
+			echo "Starting npm run commands..." && screen -S $miner_screen_name -p 0 -X stuff "SERVER_PORT=$miner_server_port npm run commands^M" && sleep 2
 
 			while :; do
 			cmd_pool=$(screen -x $miner_screen_name -X hardcopy /tmp/cmd_pool && cat /tmp/cmd_pool | grep Command: | tail -n 1)
-				if [[ -z $cmd_pool ]]; then echo "Waiting for [Command:]..." && sleep 2; else echo "Sending command 10..." && screen -S $miner_screen_name -p 0 -X stuff "10^M"; break; fi
+				if [[ -z $cmd_pool ]]; then echo "Waiting for [Command:]..." && sleep 1; else echo "Sending command 10..." && screen -S $miner_screen_name -p 0 -X stuff "10^M"; break; fi
 			done
 
 			while :; do
 			select_pool=$(screen -x $miner_screen_name -X hardcopy /tmp/select_pool && cat /tmp/select_pool | grep y/n | tail -n 1)
-				if [[ -z $select_pool ]]; then echo "Waiting for [y/n]..." && sleep 3; else echo "Confirming pool selection [y]..." && screen -S $miner_screen_name -p 0 -X stuff "y^M" && echo "Miner started!"; break; fi
+				if [[ -z $select_pool ]]; then echo "Waiting for [y/n]..." && sleep 1; else echo "Confirming pool selection [y]..." && screen -S $miner_screen_name -p 0 -X stuff "y^M" && echo "Miner started!" && rm /tmp/cmd_pool && rm /tmp/select_pool; break; fi
 			done
 		else
 			echo "CPU usage is: $cpu_usage%"
@@ -36,16 +36,16 @@ elif [[ $(uname -r | cut -d '-' -f3) == Microsoft ]]; then # Windows 10 Ubuntu S
 		if [[ $cpu_usage -le 25  ]]; then
 			echo "Low_CPU_usage_detected: $cpu_usage%"
 			echo "Sending CTRL+C..." && screen -S $miner_screen_name -p 0 -X stuff "^C" && sleep 2
-			echo "Starting npm run commands..." && screen -S $miner_screen_name -p 0 -X stuff "SERVER_PORT=$miner_server_port npm run commands^M"
+			echo "Starting npm run commands..." && screen -S $miner_screen_name -p 0 -X stuff "SERVER_PORT=$miner_server_port npm run commands^M" && sleep 2
 
 			while :; do
 			cmd_pool=$(screen -x $miner_screen_name -X hardcopy /tmp/cmd_pool && cat /tmp/cmd_pool | grep Command: | tail -n 1)
-				if [[ -z $cmd_pool ]]; then echo "Waiting for [Command:]..." && sleep 2; else echo "Sending command 10..." && screen -S $miner_screen_name -p 0 -X stuff "10^M"; break; fi
+				if [[ -z $cmd_pool ]]; then echo "Waiting for [Command:]..." && sleep 1; else echo "Sending command 10..." && screen -S $miner_screen_name -p 0 -X stuff "10^M"; break; fi
 			done
 
 			while :; do
 			select_pool=$(screen -x $miner_screen_name -X hardcopy /tmp/select_pool && cat /tmp/select_pool | grep y/n | tail -n 1)
-				if [[ -z $select_pool ]]; then echo "Waiting for [y/n]..." && sleep 3; else echo "Confirming pool selection [y]..." && screen -S $miner_screen_name -p 0 -X stuff "y^M" && echo "Miner started!"; break; fi
+				if [[ -z $select_pool ]]; then echo "Waiting for [y/n]..." && sleep 1; else echo "Confirming pool selection [y]..." && screen -S $miner_screen_name -p 0 -X stuff "y^M" && echo "Miner started!" && rm /tmp/cmd_pool && rm /tmp/select_pool; break; fi
 			done
 		else
 			echo "CPU usage is: $cpu_usage%"
